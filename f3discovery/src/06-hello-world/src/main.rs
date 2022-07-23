d5bd0c3cc11db0c12d774bc5f47c40c3a60cda62
Tomás Apolónia <tomas.apolonia@gmail.com>
2022-07-23 17:52:16 +0100

    modified:   f3discovery/src/06-hello-world/src/main.rs

--- f3discovery/src/06-hello-world/src/main.rs
+++ f3discovery/src/06-hello-world/src/main.rs
@@ -9,7 +9,14 @@ use aux6::{entry, iprint, iprintln};
 fn main() -> ! {
     let mut itm = aux6::init();
 
-    iprintln!(&mut itm.stim[0], "Hello, world!");
+    iprintln!(&mut itm.stim[0], "Hello, world!");  
+    /*
+    iprintln macro will format messages and output 
+    them to the microntrollers ITM and (SWD) Serial Wire Debug
+    */
+
+    //there is an issue with the parsing of openocd when receiving the print statement 
+    //itmdump
 
     loop {}
 }
